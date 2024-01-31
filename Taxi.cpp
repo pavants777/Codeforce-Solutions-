ca47302ad7ad9554b4d9181a6aa6f97cd5fbd414
Pavan Gowda T S <115703185+pavants777@users.noreply.github.com>
2024-01-31 07:32:53 +0530

    Add files via upload

---
+++ Taxi.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, s;
+    vector<int> groups(5, 0);
+
+    cin >> n;
+
+    while(n--) {
+        cin >> s;
+        groups[s]++;
+    }
+
+    int total = groups[4] + groups[3] + groups[2] / 2;
+
+    groups[1] -= groups[3];
+
+    if(groups[2] % 2 == 1) {
+        total += 1;
+        groups[1] -= 2;
+    }
+
+    if(groups[1] > 0) {
+        total += (groups[1] + 3) / 4;
+    }
+
+    cout << total << endl;
+    return 0;
+}
+
+
+// 8
+// 2 3 4 4 2 1 3 1
